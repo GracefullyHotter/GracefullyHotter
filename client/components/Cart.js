19693cdcaf7023dd1408d057899c43abb26b4ac4
Hernan Berisso <hberisso1@gmail.com>
2021-06-18 17:17:04 -0400

    feat: updated cart component to accept array of cart items

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -1,12 +1,41 @@
 import React from "react"
 // import { connect } from "react-redux"
 
+const cart = {
+	id: 3,
+	sauces: [
+		{
+			id: 1,
+			name: "HotSauce1",
+			imageUrl:
+				"https://hotsaucefever.com/images/sauces/654/el-yucateco-black-label-chile-habanero.jpg",
+			price: 450,
+			quantity: 4,
+		},
+		{
+			id: 2,
+			name: "HotSauce2",
+			imageUrl:
+				"https://hotsaucefever.com/images/sauces/654/el-yucateco-black-label-chile-habanero.jpg",
+			price: 400,
+			quantity: 4,
+		},
+		{
+			id: 3,
+			name: "HotSauce3",
+			imageUrl:
+				"https://hotsaucefever.com/images/sauces/654/el-yucateco-black-label-chile-habanero.jpg",
+			price: 560,
+			quantity: 4,
+		},
+	],
+}
+
 class Cart extends React.Component {
 	constructor() {
 		super()
-		// this.state = {
-		// 	loading: true,
-		// }
+		this.state = { quantity: 1 }
+		this.onClick = this.onClick.bind(this)
 	}
 
 	// componentDidMount() {
@@ -20,64 +49,80 @@ class Cart extends React.Component {
 	// 		[event.target.name]: event.target.value,
 	// 	})
 	// }
+	onClick(type) {
+		this.setState((prevState) => {
+			return {
+				quantity:
+					type == "add" ? prevState.quantity + 1 : prevState.quantity - 1,
+			}
+		})
+	}
 
 	render() {
-		// if (loading) {
-		// 	return <div>Loading...</div>
-		// }
-
+		const sauces = cart.sauces
 		return (
 			<>
 				<h1
-					style={{ textAlign: "center", margin: "auto", marginBottom: "40px" }}
+					style={{
+						textAlign: "center",
+						margin: "auto",
+						marginBottom: "40px",
+						fontSize: "50px",
+					}}
 				>
 					Cart
 				</h1>
-				{/* {cart.length > 0 ? ( */}
-				<div
-					style={{
-						display: "flex",
-						justifyContent: "center",
-						alignItems: "center",
-						margin: "auto",
-						padding: "20 50 20 50",
-						height: "​100vh",
-						width: "200px",
-						border: "1px solid black",
-					}}
-				>
-					{/* {cart.map((cartItem) => ( */}
-					<div>
+				{sauces.map((sauce) => (
+					<div
+						style={{
+							display: "flex",
+							justifyContent: "center",
+							alignItems: "center",
+							paddingTop: "20px",
+							paddingBottom: "20px",
+							marginBottom: "20px",
+							marginLeft: "auto",
+							marginRight: "auto",
+							padding: "20 50 20 50",
+							width: "500px",
+							border: "1px solid black",
+							borderRadius: "15px",
+						}}
+					>
 						<div>
-							<span>Hot Sauce Name</span>
-						</div>
+							<div>
+								<span>{sauce.name}</span>
+							</div>
+							<div>
+								<img src={sauce.imageURL} />
+							</div>
 
-						<div>
-							Image
-							{/* <img src={sauce.imageURL} alt="" /> */}
+							<div>
+								<button
+									type="button"
+									onClick={this.onClick.bind(this, "sub")}
+									name="button"
+								>
+									<span> - </span>
+								</button>
+								<span> {this.state.quantity} </span>
+								<button
+									type="button"
+									onClick={this.onClick.bind(this, "add")}
+									name="button"
+								>
+									<span> + </span>
+								</button>
+								<div>
+									<span style={{ textAlign: "center" }}>
+										${((sauce.price / 100) * this.state.quantity).toFixed(2)}
+									</span>
+								</div>
+							</div>
 						</div>
-
-						<div>
-							<span>Description</span>
-						</div>
-
-						<div>
-							<button type="button" name="button">
-								<span> - </span>
-							</button>
-							<span>quantity</span>
-							<button type="button" name="button">
-								<span> + </span>
-							</button>
-						</div>
-
-						<div>$</div>
 					</div>
-				</div>
-				{/* ))} */}
-				{/* ) : (
-					<h1>Empty Cart</h1>
-				)} */}
+				))}
+
 				<div
 					style={{
 						display: "flex",
