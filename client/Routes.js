39904f5f2f17cd76f06cdd947d4c8ef901d6c962
Hernan Berisso <hberisso1@gmail.com>
2021-06-22 15:31:26 -0400

    feat: integrated stripe checkout

--- client/Routes.js
+++ client/Routes.js
@@ -5,7 +5,6 @@ import { Login, Signup } from "./components/AuthForm"
 import LandingPage from "./components/LandingPage"
 import AllSauces from "./components/AllSauces"
 import Cart from "./components/Cart"
-import Confirmation from "./components/Confirmation"
 import Checkout from "./components/Checkout"
 import SingleSauce from "./components/SingleSauce"
 import { me } from "./store"
@@ -16,8 +15,8 @@ import Profile from "./components/Profile"
 import AllUsers from "./components/AllUsers"
 import EditUser from "./components/EditUser"
 
-import OrderHistory from "./components/OrderHistory";
-import SingleOrder from "./components/SingleOrder";
+import OrderHistory from "./components/OrderHistory"
+import SingleOrder from "./components/SingleOrder"
 
 /**
  * COMPONENT
@@ -31,37 +30,36 @@ class Routes extends Component {
 	render() {
 		const { isAdmin } = this.props
 
-    return (
-      <div>
-        <Switch>
-          <Route path="/" exact component={LandingPage} />
-          <Route path="/login" component={Login} />
-          <Route path="/signup" component={Signup} />
-          <Route exact path="/profile" component={Profile} />
-          <Route exact path="/orders" component={OrderHistory} />
-          <Route path="/orders/:id" component={SingleOrder} />
-          <Route exact path="/shop" component={AllSauces} />
-          <Route path="/cart" component={Cart} />
-          <Route path ="/confirmation" component={Confirmation} />
-          <Route path="/stripecomp" component={Checkout} />
-          <Route path="/shop/:id" component={SingleSauce} />
-          {isAdmin ? (
-            <>
-              <Route exact path="/users" component={AllUsers} />
-              <Route exact path="/users/:id/edit" component={EditUser} />
-              <Route path="/editsauce/:id" component={EditSauce} />
-              <Route exact path="/sauces/create" component={CreateSauce} />
-            </>
-          ) : (
-            <>
-              <Route path="/editsauce/:id" component={LandingPage} />
-              <Route exact path="/sauces/create" component={LandingPage} />
-            </>
-          )}
-        </Switch>
-      </div>
-    );
-  }
+		return (
+			<div>
+				<Switch>
+					<Route path="/" exact component={LandingPage} />
+					<Route path="/login" component={Login} />
+					<Route path="/signup" component={Signup} />
+					<Route exact path="/profile" component={Profile} />
+					<Route exact path="/orders" component={OrderHistory} />
+					<Route path="/orders/:id" component={SingleOrder} />
+					<Route exact path="/shop" component={AllSauces} />
+					<Route path="/cart" component={Cart} />
+					<Route path="/checkout" component={Checkout} />
+					<Route path="/shop/:id" component={SingleSauce} />
+					{isAdmin ? (
+						<>
+							<Route exact path="/users" component={AllUsers} />
+							<Route exact path="/users/:id/edit" component={EditUser} />
+							<Route path="/editsauce/:id" component={EditSauce} />
+							<Route exact path="/sauces/create" component={CreateSauce} />
+						</>
+					) : (
+						<>
+							<Route path="/editsauce/:id" component={LandingPage} />
+							<Route exact path="/sauces/create" component={LandingPage} />
+						</>
+					)}
+				</Switch>
+			</div>
+		)
+	}
 }
 
 /**
--- client/components/Checkout.js
+++ client/components/Checkout.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react"
 import { loadStripe } from "@stripe/stripe-js"
+import axios from "axios"
 // import "./App.css";
 // Make sure to call `loadStripe` outside of a component’s render to avoid
 // recreating the `Stripe` object on every render.
@@ -7,25 +8,34 @@ const stripePromise = loadStripe(
 	"pk_test_51J4aqTLayjJkaX4aO6jdnlMHpy5nM4L7cgpTq9Ke2mqDDq3QKT0mfG140yDHbFvD6VK8ZDbt06D9ZKVUTiBQQaPR0059PnEw0Z"
 )
 
+const cart = JSON.parse(localStorage.getItem("cart"))
+console.log(cart)
+
 const ProductDisplay = ({ handleClick }) => (
-	<section>
-		<div className="product">
-			<img
-				src="https://i.imgur.com/EHyR2nP.png"
-				alt="The cover of Stubborn Attachments"
-			/>
-			<div className="description">
-				<h3>Stubborn Attachments</h3>
-				<h5>$20.00</h5>
+	<section style={{ textAlign: "center", margin: "auto" }}>
+		<h1 style={{ textAlign: "center", margin: "auto" }}>Checkout</h1>
+		{cart.map((item, idx) => (
+			<div style={{ margin: "100px" }} key={item.id}>
+				<div className="product">
+					<img
+						style={{ width: "200px", height: "350px" }}
+						src={item.imageURL}
+					/>
+					<div className="description">
+						<h3>{item.name}</h3>
+						<h5>${((item.price / 100) * item.quantity).toFixed(2)}</h5>
+					</div>
+				</div>
 			</div>
-		</div>
+		))}
 		<button
+			style={{ marginBottom: "100px" }}
 			type="button"
 			id="checkout-button"
 			role="link"
 			onClick={handleClick}
 		>
-			Checkout
+			Proceed To Payment
 		</button>
 	</section>
 )
@@ -57,11 +67,9 @@ export default function App() {
 	const handleClick = async (event) => {
 		const stripe = await stripePromise
 
-		const response = await fetch("/create-checkout-session", {
-			method: "POST",
-		})
-
-		const session = await response.json()
+		const response = await axios.post("/create-checkout-session", cart)
+		console.log(response)
+		const session = await response.data
 
 		// When the customer clicks on the button, redirect them to Checkout.
 		const result = await stripe.redirectToCheckout({
