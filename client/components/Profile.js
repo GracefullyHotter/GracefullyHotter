592d299586ccb251e130fba7e390cecf405d8f59
helsung <helenjeehosung@gmail.com>
2021-06-22 22:31:19 -0400

    fix: button styling

--- client/components/Profile.js
+++ client/components/Profile.js
@@ -13,12 +13,18 @@ class Profile extends React.Component {
     return (
       <React.Fragment>
         <div id="user-info">
-          <h1>{user.name}</h1>
-          <h1>{user.email}</h1>
-          <Link to={`/users/profiles/edit/${user.id}`}>Edit My Profile</Link>
+          <h1 className="title is-3 is-centered">ACCOUNT INFORMATION</h1>
+          <h2>{user.name}</h2>
+          <h2>{user.email}</h2>
+          <Link
+            className="button is-medium is-link"
+            to={`/users/profiles/edit/${user.id}`}
+          >
+            Edit My Profile
+          </Link>
         </div>
 
-        <a>View Order History</a>
+        <a className="button is-medium is-danger">Order History</a>
 
         <br></br>
         <br></br>
